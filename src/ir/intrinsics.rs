46ab00bd4fb19981ebfaccce831e036080461b38
jordan <jordan.stachura@gmail.com>
2024-01-11 00:19:39 -0500

    intrinsics

--- src/ir/intrinsics.rs
+++ src/ir/intrinsics.rs
@@ -1,10 +1,18 @@
-/*
 
-    LLVMLookupIntrinsicID⚠
+use llvm_sys::core as llvm;
 
-    LLVMIntrinsicCopyOverloadedName2⚠
-    LLVMIntrinsicGetName⚠
-    LLVMIntrinsicGetType⚠
-    LLVMIntrinsicIsOverloaded⚠
+// TODO Docs
+pub fn lookup_intrinsic_id(name: &str) -> u32 {
+    unsafe { llvm::LLVMLookupIntrinsicID(str_to_cstr!(name), name.len()) }
+}
 
-*/
+// TODO Docs
+pub fn get_intrinsic_name(id: u32) -> String {
+    let ref mut name: Vec<usize> = vec![];
+    cstr_to_str!(llvm::LLVMIntrinsicGetName(id, name.as_mut_ptr())).to_string()
+}
+
+// TODO Docs
+pub fn intrinsic_is_overloaded(id: u32) -> bool {
+    bool_to_rust!(llvm::LLVMIntrinsicIsOverloaded(id))
+}
