f8938d3357a16f79a34bdade6279a371e1ab9a43
jordan <jordan.stachura@gmail.com>
2024-01-10 23:53:01 -0500

    metadata

--- src/ir/metadata.rs
+++ src/ir/metadata.rs
@@ -3,14 +3,17 @@ use llvm_sys::prelude::*;
 
 use crate::wrapper::Wrapper;
 
+
 // Wrapper for actual ref to metadata
 wrapper!(ActualMetadata, LLVMMetadataRef);
-
 
 // Metadata as type
 wrapper!(MetadataType, LLVMTypeRef);
 impl super::types::Type for MetadataType {}
 
-
 // Metadata as value
 pub use super::values::MetadataAsValue;
+
+
+// Attribute
+wrapper!(Attribute, LLVMAttributeRef);
